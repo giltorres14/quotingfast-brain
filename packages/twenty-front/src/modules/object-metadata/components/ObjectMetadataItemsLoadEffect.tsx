giltorres14/quotingfast-brain

commit 6b1bb82116cd639a91f5fc57250938021ed5425d

Fix tasks (#5199)
## Query depth deprecation

I'm deprecating depth parameter in our graphql query / cache tooling.
They were obsolete since we introduce the possibility to provide
RecordGqlFields

## Refactor combinedFindManyRecordHook

The hook can now take an array of operationSignatures

## Fix tasks issues

Fix optimistic rendering issue. Note that we still haven't handle
optimisticEffect on creation properly

packages/twenty-front/src/modules/object-metadata/components/ObjectMetadataItemsLoadEffect.tsx
@@ -4,6 +4,7 @@ import { useRecoilState, useRecoilValue } from 'recoil';
 import { currentUserState } from '@/auth/states/currentUserState';
 import { useFindManyObjectMetadataItems } from '@/object-metadata/hooks/useFindManyObjectMetadataItems';
 import { objectMetadataItemsState } from '@/object-metadata/states/objectMetadataItemsState';
+import { getObjectMetadataItemsMock } from '@/object-metadata/utils/getObjectMetadataItemsMock';
 import { isDeeplyEqual } from '~/utils/isDeeplyEqual';
 import { isUndefinedOrNull } from '~/utils/isUndefinedOrNull';
 
@@ -19,10 +20,18 @@ export const ObjectMetadataItemsLoadEffect = () => {
   );
 
   useEffect(() => {
-    if (!isDeeplyEqual(objectMetadataItems, newObjectMetadataItems)) {
-      setObjectMetadataItems(newObjectMetadataItems);
+    const toSetObjectMetadataItems = isUndefinedOrNull(currentUser)
+      ? getObjectMetadataItemsMock()
+      : newObjectMetadataItems;
+    if (!isDeeplyEqual(objectMetadataItems, toSetObjectMetadataItems)) {
+      setObjectMetadataItems(toSetObjectMetadataItems);
     }
-  }, [newObjectMetadataItems, objectMetadataItems, setObjectMetadataItems]);
+  }, [
+    currentUser,
+    newObjectMetadataItems,
+    objectMetadataItems,
+    setObjectMetadataItems,
+  ]);
 
   return <></>;
 };

commit 4a6e20a0b45943a52e1e4f082031af5cd2e40092

Fix sign up (#5231)
Sign up was broken by #5199

packages/twenty-front/src/modules/object-metadata/components/ObjectMetadataItemsLoadEffect.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 
 import { currentUserState } from '@/auth/states/currentUserState';
+import { currentWorkspaceState } from '@/auth/states/currentWorkspaceState';
 import { useFindManyObjectMetadataItems } from '@/object-metadata/hooks/useFindManyObjectMetadataItems';
 import { objectMetadataItemsState } from '@/object-metadata/states/objectMetadataItemsState';
 import { getObjectMetadataItemsMock } from '@/object-metadata/utils/getObjectMetadataItemsMock';
@@ -10,6 +11,8 @@ import { isUndefinedOrNull } from '~/utils/isUndefinedOrNull';
 
 export const ObjectMetadataItemsLoadEffect = () => {
   const currentUser = useRecoilValue(currentUserState);
+  const currentWorkspace = useRecoilValue(currentWorkspaceState);
+
   const { objectMetadataItems: newObjectMetadataItems } =
     useFindManyObjectMetadataItems({
       skip: isUndefinedOrNull(currentUser),
@@ -20,14 +23,17 @@ export const ObjectMetadataItemsLoadEffect = () => {
   );
 
   useEffect(() => {
-    const toSetObjectMetadataItems = isUndefinedOrNull(currentUser)
-      ? getObjectMetadataItemsMock()
-      : newObjectMetadataItems;
+    const toSetObjectMetadataItems =
+      isUndefinedOrNull(currentUser) ||
+      currentWorkspace?.activationStatus !== 'active'
+        ? getObjectMetadataItemsMock()
+        : newObjectMetadataItems;
     if (!isDeeplyEqual(objectMetadataItems, toSetObjectMetadataItems)) {
       setObjectMetadataItems(toSetObjectMetadataItems);
     }
   }, [
     currentUser,
+    currentWorkspace?.activationStatus,
     newObjectMetadataItems,
     objectMetadataItems,
     setObjectMetadataItems,