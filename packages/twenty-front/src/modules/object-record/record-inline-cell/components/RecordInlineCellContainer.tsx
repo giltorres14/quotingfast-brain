twentyhq/twenty

commit 679530020c13927b20daec818711b0a07f9214ee

increase recordCard field hovering zone width + click (recordCard and summaryCard) (#11983)
PR preview


https://github.com/user-attachments/assets/7d90f384-6bee-41d7-9aec-cadb434e7db6



closes https://github.com/twentyhq/twenty/issues/10561

packages/twenty-front/src/modules/object-record/record-inline-cell/components/RecordInlineCellContainer.tsx
@@ -9,6 +9,7 @@ import { getRecordFieldInputId } from '@/object-record/utils/getRecordFieldInput
 
 import { assertFieldMetadata } from '@/object-record/record-field/types/guards/assertFieldMetadata';
 import { isFieldText } from '@/object-record/record-field/types/guards/isFieldText';
+import { useInlineCell } from '@/object-record/record-inline-cell/hooks/useInlineCell';
 import {
   AppTooltip,
   OverflowingTextWithTooltip,
@@ -86,8 +87,14 @@ export const StyledSkeletonDiv = styled.div`
 `;
 
 export const RecordInlineCellContainer = () => {
-  const { readonly, IconLabel, label, labelWidth, showLabel } =
-    useRecordInlineCellContext();
+  const {
+    readonly,
+    IconLabel,
+    label,
+    labelWidth,
+    showLabel,
+    editModeContentOnly,
+  } = useRecordInlineCellContext();
 
   const { recordId, fieldDefinition } = useContext(FieldContext);
 
@@ -96,6 +103,14 @@ export const RecordInlineCellContainer = () => {
   }
 
   const { setIsFocused } = useFieldFocus();
+
+  const { openInlineCell } = useInlineCell();
+
+  const handleDisplayModeClick = () => {
+    if (!readonly && !editModeContentOnly) {
+      openInlineCell();
+    }
+  };
 
   const handleContainerMouseEnter = () => {
     if (!readonly) {
@@ -119,6 +134,7 @@ export const RecordInlineCellContainer = () => {
     <StyledInlineCellBaseContainer
       onMouseEnter={handleContainerMouseEnter}
       onMouseLeave={handleContainerMouseLeave}
+      onClick={handleDisplayModeClick}
     >
       {(IconLabel || label) && (
         <StyledLabelAndIconContainer id={labelId}>

packages/twenty-front/src/modules/object-record/record-inline-cell/components/RecordInlineCellValue.tsx
@@ -39,13 +39,7 @@ export const RecordInlineCellValue = () => {
     isCentered,
   } = useRecordInlineCellContext();
 
-  const { isInlineCellInEditMode, openInlineCell } = useInlineCell();
-
-  const handleDisplayModeClick = () => {
-    if (!readonly && !editModeContentOnly) {
-      openInlineCell();
-    }
-  };
+  const { isInlineCellInEditMode } = useInlineCell();
 
   if (loading === true) {
     return <RecordInlineCellSkeletonLoader />;
@@ -63,11 +57,7 @@ export const RecordInlineCellValue = () => {
           </RecordInlineCellDisplayMode>
         </StyledClickableContainer>
       ) : (
-        <StyledClickableContainer
-          readonly={readonly}
-          onClick={handleDisplayModeClick}
-          isCentered={isCentered}
-        >
+        <StyledClickableContainer readonly={readonly} isCentered={isCentered}>
           <RecordInlineCellDisplayMode>
             {displayModeContent}
           </RecordInlineCellDisplayMode>

packages/twenty-front/src/modules/object-record/record-board/record-board-card/components/StopPropagationContainer.tsx
@@ -1,11 +1,10 @@
-import { ReactNode } from 'react';
 import styled from '@emotion/styled';
+import { ReactNode } from 'react';
 
 const StyledFieldContainer = styled.div`
   display: flex;
   flex-direction: row;
-  width: fit-content;
-  max-width: 100%;
+  width: 100%;
 `;
 
 export const StopPropagationContainer = ({

giltorres14/quotingfast-brain

commit 36d3f534d56be01c5dc5da6041f4c53319285187

Fix composite field edition (#12095)
Fixes https://github.com/twentyhq/twenty/issues/12056

Bug has been introduced by
https://github.com/twentyhq/twenty/pull/11983/files

Adding onClick on the container makes it always available. Before it was
only when not in edit mode.
It breaks the click outside of the `RecordInlineCell`.

This PR set the onClick only when not in edit mode.



https://github.com/user-attachments/assets/a44337a9-72e6-4de8-afa1-95de6b953459

packages/twenty-front/src/modules/object-record/record-inline-cell/components/RecordInlineCellContainer.tsx
@@ -43,7 +43,6 @@ const StyledLabelAndIconContainer = styled.div`
 `;
 
 const StyledValueContainer = styled.div<{ readonly: boolean }>`
-  cursor: ${({ readonly }) => (readonly ? 'default' : 'pointer')};
   display: flex;
   min-width: 0;
   position: relative;
@@ -72,7 +71,7 @@ const StyledLabelContainer = styled.div<{ width?: number }>`
   width: ${({ width }) => width}px;
 `;
 
-const StyledInlineCellBaseContainer = styled.div`
+const StyledInlineCellBaseContainer = styled.div<{ readonly: boolean }>`
   box-sizing: border-box;
   width: 100%;
   display: flex;
@@ -80,6 +79,7 @@ const StyledInlineCellBaseContainer = styled.div`
   gap: ${({ theme }) => theme.spacing(1)};
   user-select: none;
   align-items: center;
+  cursor: ${({ readonly }) => (readonly ? 'default' : 'pointer')};
 `;
 
 export const StyledSkeletonDiv = styled.div`
@@ -96,21 +96,18 @@ export const RecordInlineCellContainer = () => {
     editModeContentOnly,
   } = useRecordInlineCellContext();
 
+  const { isInlineCellInEditMode, openInlineCell } = useInlineCell();
+
   const { recordId, fieldDefinition } = useContext(FieldContext);
+
+  const shouldContainerBeClickable =
+    !readonly && !editModeContentOnly && !isInlineCellInEditMode;
 
   if (isFieldText(fieldDefinition)) {
     assertFieldMetadata(FieldMetadataType.TEXT, isFieldText, fieldDefinition);
   }
 
   const { setIsFocused } = useFieldFocus();
-
-  const { openInlineCell } = useInlineCell();
-
-  const handleDisplayModeClick = () => {
-    if (!readonly && !editModeContentOnly) {
-      openInlineCell();
-    }
-  };
 
   const handleContainerMouseEnter = () => {
     if (!readonly) {
@@ -132,9 +129,10 @@ export const RecordInlineCellContainer = () => {
 
   return (
     <StyledInlineCellBaseContainer
+      readonly={readonly ?? false}
       onMouseEnter={handleContainerMouseEnter}
       onMouseLeave={handleContainerMouseLeave}
-      onClick={handleDisplayModeClick}
+      onClick={shouldContainerBeClickable ? openInlineCell : undefined}
     >
       {(IconLabel || label) && (
         <StyledLabelAndIconContainer id={labelId}>

packages/twenty-front/src/modules/object-record/record-inline-cell/components/RecordInlineCellValue.tsx
@@ -50,19 +50,11 @@ export const RecordInlineCellValue = () => {
       {!readonly && isInlineCellInEditMode && (
         <RecordInlineCellEditMode>{editModeContent}</RecordInlineCellEditMode>
       )}
-      {editModeContentOnly ? (
-        <StyledClickableContainer readonly={readonly} isCentered={isCentered}>
-          <RecordInlineCellDisplayMode>
-            {editModeContent}
-          </RecordInlineCellDisplayMode>
-        </StyledClickableContainer>
-      ) : (
-        <StyledClickableContainer readonly={readonly} isCentered={isCentered}>
-          <RecordInlineCellDisplayMode>
-            {displayModeContent}
-          </RecordInlineCellDisplayMode>
-        </StyledClickableContainer>
-      )}
+      <StyledClickableContainer readonly={readonly} isCentered={isCentered}>
+        <RecordInlineCellDisplayMode>
+          {editModeContentOnly ? editModeContent : displayModeContent}
+        </RecordInlineCellDisplayMode>
+      </StyledClickableContainer>
     </>
   );
 };