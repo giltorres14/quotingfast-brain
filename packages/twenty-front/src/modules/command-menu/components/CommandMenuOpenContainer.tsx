twentyhq/twenty

commit 6d6738e7cb3d75de79ed64b515de29ff9ba5e34d

Fixes on side panel closing and table rows and board cards activation (#12609)
Fixes https://github.com/twentyhq/core-team-issues/issues/1096

This PR:
- Prevents interaction with elements inside the index page when the side
panel is opened, except for switching between records
- Prevents stacking multiple records in the side panel navigation stack
when navigating from the index
- Adds activation and unfocus logic for board cards when clicked
- Fixes table row activation after clicking on a record chip

Before:


https://github.com/user-attachments/assets/dcfec9fb-392b-4760-9b11-b0f077087b82


After:


https://github.com/user-attachments/assets/93e0dc6a-c693-4484-b23e-f5ae291eb472

packages/twenty-front/src/modules/command-menu/components/CommandMenuOpenContainer.tsx
@@ -51,12 +51,14 @@ export const CommandMenuOpenContainer = ({
 
   const handleClickOutside = useRecoilCallback(
     ({ snapshot }) =>
-      () => {
+      (event: MouseEvent | TouchEvent) => {
         const hotkeyScope = snapshot
           .getLoadable(currentHotkeyScopeState)
           .getValue();
 
         if (hotkeyScope?.scope === CommandMenuHotkeyScope.CommandMenuFocused) {
+          event.stopImmediatePropagation();
+          event.preventDefault();
           closeCommandMenu();
         }
       },

packages/twenty-front/src/modules/command-menu/hooks/useOpenRecordInCommandMenu.ts
@@ -38,10 +38,12 @@ export const useOpenRecordInCommandMenu = () => {
         recordId,
         objectNameSingular,
         isNewRecord = false,
+        resetNavigationStack = false,
       }: {
         recordId: string;
         objectNameSingular: string;
         isNewRecord?: boolean;
+        resetNavigationStack?: boolean;
       }) => {
         const navigationStack = getSnapshotValue(
           snapshot,
@@ -171,7 +173,7 @@ export const useOpenRecordInCommandMenu = () => {
           pageIcon: Icon,
           pageIconColor: IconColor,
           pageId: pageComponentInstanceId,
-          resetNavigationStack: false,
+          resetNavigationStack,
         });
 
         if (objectNameSingular === CoreObjectNameSingular.WorkflowRun) {

packages/twenty-front/src/modules/object-record/record-board/record-board-card/components/RecordBoardCard.tsx
@@ -10,6 +10,8 @@ import { isRecordBoardCompactModeActiveComponentState } from '@/object-record/re
 import { recordBoardVisibleFieldDefinitionsComponentSelector } from '@/object-record/record-board/states/selectors/recordBoardVisibleFieldDefinitionsComponentSelector';
 
 import { ActionMenuDropdownHotkeyScope } from '@/action-menu/types/ActionMenuDropdownHotKeyScope';
+import { useActiveRecordBoardCard } from '@/object-record/record-board/hooks/useActiveRecordBoardCard';
+import { useFocusedRecordBoardCard } from '@/object-record/record-board/hooks/useFocusedRecordBoardCard';
 import { RecordBoardCardBody } from '@/object-record/record-board/record-board-card/components/RecordBoardCardBody';
 import { RecordBoardCardHeader } from '@/object-record/record-board/record-board-card/components/RecordBoardCardHeader';
 import { RECORD_BOARD_CARD_CLICK_OUTSIDE_ID } from '@/object-record/record-board/record-board-card/constants/RecordBoardCardClickOutsideId';
@@ -142,6 +144,8 @@ export const RecordBoardCard = () => {
   const { openDropdown } = useDropdownV2();
 
   const { openRecordFromIndexView } = useOpenRecordFromIndexView();
+  const { activateBoardCard } = useActiveRecordBoardCard(recordBoardId);
+  const { unfocusBoardCard } = useFocusedRecordBoardCard(recordBoardId);
 
   const handleActionMenuDropdown = (event: React.MouseEvent) => {
     event.preventDefault();
@@ -156,6 +160,8 @@ export const RecordBoardCard = () => {
   };
 
   const handleCardClick = () => {
+    activateBoardCard({ rowIndex, columnIndex });
+    unfocusBoardCard();
     openRecordFromIndexView({ recordId });
   };
 

packages/twenty-front/src/modules/object-record/record-board/record-board-card/components/RecordBoardCardHeader.tsx
@@ -4,15 +4,17 @@ import { useRecordBoardSelection } from '@/object-record/record-board/hooks/useR
 import { RecordBoardCardHeaderContainer } from '@/object-record/record-board/record-board-card/components/RecordBoardCardHeaderContainer';
 import { StopPropagationContainer } from '@/object-record/record-board/record-board-card/components/StopPropagationContainer';
 import { RecordBoardCardContext } from '@/object-record/record-board/record-board-card/contexts/RecordBoardCardContext';
-import { RecordBoardScopeInternalContext } from '@/object-record/record-board/scopes/scope-internal-context/RecordBoardScopeInternalContext';
 import { isRecordBoardCardSelectedComponentFamilyState } from '@/object-record/record-board/states/isRecordBoardCardSelectedComponentFamilyState';
 import { isRecordBoardCompactModeActiveComponentState } from '@/object-record/record-board/states/isRecordBoardCompactModeActiveComponentState';
 
+import { useActiveRecordBoardCard } from '@/object-record/record-board/hooks/useActiveRecordBoardCard';
+import { useFocusedRecordBoardCard } from '@/object-record/record-board/hooks/useFocusedRecordBoardCard';
 import { useOpenRecordFromIndexView } from '@/object-record/record-index/hooks/useOpenRecordFromIndexView';
+import { recordIndexOpenRecordInState } from '@/object-record/record-index/states/recordIndexOpenRecordInState';
 import { recordStoreFamilyState } from '@/object-record/record-store/states/recordStoreFamilyState';
-import { useAvailableScopeIdOrThrow } from '@/ui/utilities/recoil-scope/scopes-internal/hooks/useAvailableScopeId';
 import { useRecoilComponentFamilyStateV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentFamilyStateV2';
 import { useRecoilComponentValueV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentValueV2';
+import { ViewOpenRecordInType } from '@/views/types/ViewOpenRecordInType';
 import styled from '@emotion/styled';
 import { Dispatch, SetStateAction, useContext } from 'react';
 import { useRecoilValue } from 'recoil';
@@ -45,11 +47,10 @@ export const RecordBoardCardHeader = ({
 
   const record = useRecoilValue(recordStoreFamilyState(recordId));
 
-  const { objectMetadataItem } = useContext(RecordBoardContext);
-
-  const recordBoardId = useAvailableScopeIdOrThrow(
-    RecordBoardScopeInternalContext,
-  );
+  const { objectMetadataItem, recordBoardId } = useContext(RecordBoardContext);
+  const { rowIndex, columnIndex } = useContext(RecordBoardCardContext);
+  const { activateBoardCard } = useActiveRecordBoardCard(recordBoardId);
+  const { unfocusBoardCard } = useFocusedRecordBoardCard(recordBoardId);
 
   const showCompactView = useRecoilComponentValueV2(
     isRecordBoardCompactModeActiveComponentState,
@@ -66,6 +67,12 @@ export const RecordBoardCardHeader = ({
 
   const { openRecordFromIndexView } = useOpenRecordFromIndexView();
 
+  const recordIndexOpenRecordIn = useRecoilValue(recordIndexOpenRecordInState);
+  const triggerEvent =
+    recordIndexOpenRecordIn === ViewOpenRecordInType.SIDE_PANEL
+      ? 'CLICK'
+      : 'MOUSE_DOWN';
+
   return (
     <RecordBoardCardHeaderContainer showCompactView={showCompactView}>
       <StopPropagationContainer>
@@ -76,9 +83,11 @@ export const RecordBoardCardHeader = ({
             variant={AvatarChipVariant.Transparent}
             maxWidth={150}
             onClick={() => {
+              activateBoardCard({ rowIndex, columnIndex });
+              unfocusBoardCard();
               openRecordFromIndexView({ recordId });
             }}
-            triggerEvent="CLICK"
+            triggerEvent={triggerEvent}
           />
         )}
       </StopPropagationContainer>

packages/twenty-front/src/modules/object-record/record-index/hooks/useOpenRecordFromIndexView.ts
@@ -71,6 +71,7 @@ export const useOpenRecordFromIndexView = () => {
           openRecordInCommandMenu({
             recordId,
             objectNameSingular,
+            resetNavigationStack: true,
           });
         } else {
           navigate(AppPath.RecordShowPage, {

packages/twenty-front/src/modules/object-record/record-table/record-table-cell/components/RecordTableCellFieldContextLabelIdentifier.tsx
@@ -3,13 +3,18 @@ import { FieldContext } from '@/object-record/record-field/contexts/FieldContext
 import { useIsFieldValueReadOnly } from '@/object-record/record-field/hooks/useIsFieldValueReadOnly';
 import { useRecordIndexContextOrThrow } from '@/object-record/record-index/contexts/RecordIndexContext';
 import { useOpenRecordFromIndexView } from '@/object-record/record-index/hooks/useOpenRecordFromIndexView';
+import { recordIndexOpenRecordInState } from '@/object-record/record-index/states/recordIndexOpenRecordInState';
 import { RecordUpdateContext } from '@/object-record/record-table/contexts/EntityUpdateMutationHookContext';
 import { RecordTableCellContext } from '@/object-record/record-table/contexts/RecordTableCellContext';
 import { useRecordTableContextOrThrow } from '@/object-record/record-table/contexts/RecordTableContext';
 import { useRecordTableRowContextOrThrow } from '@/object-record/record-table/contexts/RecordTableRowContext';
+import { useActiveRecordTableRow } from '@/object-record/record-table/hooks/useActiveRecordTableRow';
+import { useFocusedRecordTableRow } from '@/object-record/record-table/hooks/useFocusedRecordTableRow';
 import { isRecordTableScrolledLeftComponentState } from '@/object-record/record-table/states/isRecordTableScrolledLeftComponentState';
 import { useRecoilComponentValueV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentValueV2';
+import { ViewOpenRecordInType } from '@/views/types/ViewOpenRecordInType';
 import { ReactNode, useContext } from 'react';
+import { useRecoilValue } from 'recoil';
 import { useIsMobile } from 'twenty-ui/utilities';
 
 type RecordTableCellFieldContextLabelIdentifierProps = {
@@ -25,7 +30,10 @@ export const RecordTableCellFieldContextLabelIdentifier = ({
     useRecordTableRowContextOrThrow();
 
   const { columnDefinition } = useContext(RecordTableCellContext);
-  const { objectMetadataItem } = useRecordTableContextOrThrow();
+  const { objectMetadataItem, recordTableId } = useRecordTableContextOrThrow();
+  const { rowIndex } = useRecordTableRowContextOrThrow();
+  const { activateRecordTableRow } = useActiveRecordTableRow(recordTableId);
+  const { unfocusRecordTableRow } = useFocusedRecordTableRow(recordTableId);
 
   const isMobile = useIsMobile();
   const isRecordTableScrolledLeftComponent = useRecoilComponentValueV2(
@@ -51,6 +59,12 @@ export const RecordTableCellFieldContextLabelIdentifier = ({
 
   const { openRecordFromIndexView } = useOpenRecordFromIndexView();
 
+  const recordIndexOpenRecordIn = useRecoilValue(recordIndexOpenRecordInState);
+  const triggerEvent =
+    recordIndexOpenRecordIn === ViewOpenRecordInType.SIDE_PANEL
+      ? 'CLICK'
+      : 'MOUSE_DOWN';
+
   return (
     <FieldContext.Provider
       value={{
@@ -64,9 +78,12 @@ export const RecordTableCellFieldContextLabelIdentifier = ({
         isReadOnly: isFieldReadOnly,
         maxWidth: columnDefinition.size,
         onRecordChipClick: () => {
+          activateRecordTableRow(rowIndex);
+          unfocusRecordTableRow();
           openRecordFromIndexView({ recordId });
         },
         isForbidden: !hasObjectReadPermissions,
+        triggerEvent,
       }}
     >
       {children}

giltorres14/quotingfast-brain

commit 02728a2e6ca97fbf20b5d0c5ff3b17c941c53ed4

Fix side panel closing when clicking on workflow nodes or options (#12629)
This bug was introduced by
https://github.com/twentyhq/twenty/pull/12609. This previous PR was made
to prevent interacting with elements behind the side panel when it is
opened. But in the workflow visualizer we need to interact with the
diagram nodes.

Fix: add the workflow visualizer nodes and options to the excluded click
outside ids of the side panel.



https://github.com/user-attachments/assets/1fb770d6-1c17-4ac1-b1a9-ed162bdbe298

packages/twenty-front/src/modules/command-menu/components/CommandMenuOpenContainer.tsx
@@ -9,6 +9,9 @@ import { RootStackingContextZIndices } from '@/ui/layout/constants/RootStackingC
 import { PAGE_HEADER_COMMAND_MENU_BUTTON_CLICK_OUTSIDE_ID } from '@/ui/layout/page-header/constants/PageHeaderCommandMenuButtonClickOutsideId';
 import { currentHotkeyScopeState } from '@/ui/utilities/hotkey/states/internal/currentHotkeyScopeState';
 import { useListenClickOutside } from '@/ui/utilities/pointer-event/hooks/useListenClickOutside';
+import { WORKFLOW_DIAGRAM_CREATE_STEP_NODE_CLICK_OUTSIDE_ID } from '@/workflow/workflow-diagram/constants/WorkflowDiagramCreateStepNodeClickOutsideId';
+import { WORKFLOW_DIAGRAM_EDGE_OPTIONS_CLICK_OUTSIDE_ID } from '@/workflow/workflow-diagram/constants/WorkflowDiagramEdgeOptionsClickOutsideId';
+import { WORKFLOW_DIAGRAM_STEP_NODE_BASE_CLICK_OUTSIDE_ID } from '@/workflow/workflow-diagram/constants/WorkflowDiagramStepNodeClickOutsideId';
 import { useTheme } from '@emotion/react';
 
 import styled from '@emotion/styled';
@@ -74,6 +77,9 @@ export const CommandMenuOpenContainer = ({
       LINK_CHIP_CLICK_OUTSIDE_ID,
       RECORD_CHIP_CLICK_OUTSIDE_ID,
       SLASH_MENU_DROPDOWN_CLICK_OUTSIDE_ID,
+      WORKFLOW_DIAGRAM_STEP_NODE_BASE_CLICK_OUTSIDE_ID,
+      WORKFLOW_DIAGRAM_EDGE_OPTIONS_CLICK_OUTSIDE_ID,
+      WORKFLOW_DIAGRAM_CREATE_STEP_NODE_CLICK_OUTSIDE_ID,
     ],
   });
 

packages/twenty-front/src/modules/workflow/workflow-diagram/components/WorkflowDiagramCreateStepNode.tsx
@@ -1,9 +1,10 @@
-import styled from '@emotion/styled';
-import { Position } from '@xyflow/react';
-import { IconButton } from 'twenty-ui/input';
-import { IconPlus } from 'twenty-ui/display';
 import { WorkflowDiagramBaseHandle } from '@/workflow/workflow-diagram/components/WorkflowDiagramBaseHandle';
 import { CREATE_STEP_NODE_WIDTH } from '@/workflow/workflow-diagram/constants/CreateStepNodeWidth';
+import { WORKFLOW_DIAGRAM_CREATE_STEP_NODE_CLICK_OUTSIDE_ID } from '@/workflow/workflow-diagram/constants/WorkflowDiagramCreateStepNodeClickOutsideId';
+import styled from '@emotion/styled';
+import { Position } from '@xyflow/react';
+import { IconPlus } from 'twenty-ui/display';
+import { IconButton } from 'twenty-ui/input';
 
 const StyledContainer = styled.div`
   left: ${CREATE_STEP_NODE_WIDTH / 2}px;
@@ -13,7 +14,9 @@ const StyledContainer = styled.div`
 
 export const WorkflowDiagramCreateStepNode = () => {
   return (
-    <StyledContainer>
+    <StyledContainer
+      data-click-outside-id={WORKFLOW_DIAGRAM_CREATE_STEP_NODE_CLICK_OUTSIDE_ID}
+    >
       <WorkflowDiagramBaseHandle type="target" position={Position.Top} />
 
       <IconButton Icon={IconPlus} size="medium" ariaLabel="Add a step" />

packages/twenty-front/src/modules/workflow/workflow-diagram/components/WorkflowDiagramEdgeOptions.tsx
@@ -1,10 +1,11 @@
-import { EdgeLabelRenderer } from '@xyflow/react';
 import { STEP_ICON_WIDTH } from '@/workflow/workflow-diagram/constants/CreateStepNodeWidth';
-import styled from '@emotion/styled';
-import { IconButtonGroup } from 'twenty-ui/input';
-import { IconPlus } from 'twenty-ui/display';
+import { WORKFLOW_DIAGRAM_EDGE_OPTIONS_CLICK_OUTSIDE_ID } from '@/workflow/workflow-diagram/constants/WorkflowDiagramEdgeOptionsClickOutsideId';
 import { useStartNodeCreation } from '@/workflow/workflow-diagram/hooks/useStartNodeCreation';
+import styled from '@emotion/styled';
+import { EdgeLabelRenderer } from '@xyflow/react';
 import { isDefined } from 'twenty-shared/utils';
+import { IconPlus } from 'twenty-ui/display';
+import { IconButtonGroup } from 'twenty-ui/input';
 
 const EDGE_OPTION_BUTTON_LEFT_MARGIN = 8;
 
@@ -38,7 +39,11 @@ export const WorkflowDiagramEdgeOptions = ({
 
   return (
     <EdgeLabelRenderer>
-      <StyledContainer labelX={labelX} labelY={labelY}>
+      <StyledContainer
+        labelX={labelX}
+        labelY={labelY}
+        data-click-outside-id={WORKFLOW_DIAGRAM_EDGE_OPTIONS_CLICK_OUTSIDE_ID}
+      >
         <StyledIconButtonGroup
           className="nodrag nopan"
           iconButtons={[

packages/twenty-front/src/modules/workflow/workflow-diagram/components/WorkflowDiagramStepNodeBase.tsx
@@ -1,4 +1,6 @@
+import { WorkflowDiagramBaseHandle } from '@/workflow/workflow-diagram/components/WorkflowDiagramBaseHandle';
 import { NODE_BORDER_WIDTH } from '@/workflow/workflow-diagram/constants/NodeBorderWidth';
+import { WORKFLOW_DIAGRAM_STEP_NODE_BASE_CLICK_OUTSIDE_ID } from '@/workflow/workflow-diagram/constants/WorkflowDiagramStepNodeClickOutsideId';
 import { WorkflowDiagramStepNodeData } from '@/workflow/workflow-diagram/types/WorkflowDiagram';
 import { WorkflowDiagramNodeVariant } from '@/workflow/workflow-diagram/types/WorkflowDiagramNodeVariant';
 import { css } from '@emotion/react';
@@ -8,7 +10,6 @@ import React from 'react';
 import { capitalize, isDefined } from 'twenty-shared/utils';
 import { Label, OverflowingTextWithTooltip } from 'twenty-ui/display';
 import { Loader } from 'twenty-ui/feedback';
-import { WorkflowDiagramBaseHandle } from '@/workflow/workflow-diagram/components/WorkflowDiagramBaseHandle';
 
 const StyledStepNodeContainer = styled.div`
   display: flex;
@@ -185,7 +186,10 @@ export const WorkflowDiagramStepNodeBase = ({
   RightFloatingElement?: React.ReactNode;
 }) => {
   return (
-    <StyledStepNodeContainer className="workflow-node-container">
+    <StyledStepNodeContainer
+      className="workflow-node-container"
+      data-click-outside-id={WORKFLOW_DIAGRAM_STEP_NODE_BASE_CLICK_OUTSIDE_ID}
+    >
       {nodeType !== 'trigger' ? (
         <WorkflowDiagramBaseHandle type="target" position={Position.Top} />
       ) : null}

packages/twenty-front/src/modules/workflow/workflow-diagram/constants/WorkflowDiagramCreateStepNodeClickOutsideId.ts
@@ -1,0 +1,2 @@
+export const WORKFLOW_DIAGRAM_CREATE_STEP_NODE_CLICK_OUTSIDE_ID =
+  'workflow-diagram-create-step-node';

packages/twenty-front/src/modules/workflow/workflow-diagram/constants/WorkflowDiagramEdgeOptionsClickOutsideId.ts
@@ -1,0 +1,2 @@
+export const WORKFLOW_DIAGRAM_EDGE_OPTIONS_CLICK_OUTSIDE_ID =
+  'workflow-diagram-edge-options';

packages/twenty-front/src/modules/workflow/workflow-diagram/constants/WorkflowDiagramStepNodeClickOutsideId.ts
@@ -1,0 +1,2 @@
+export const WORKFLOW_DIAGRAM_STEP_NODE_BASE_CLICK_OUTSIDE_ID =
+  'workflow-diagram-step-node-base';