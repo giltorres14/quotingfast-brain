91b19d95f5d270b8582608beeab81dc642bd27cd
Gil Torres <giltorres@MacBook-Pro-100.local>
2025-07-31 00:40:29 -0400

    Fix SQL syntax error in add_campaign_type migration: use single quotes for string literals

--- apps/platform/db/migrations/20230516214727_add_campaign_type.js
+++ apps/platform/db/migrations/20230516214727_add_campaign_type.js
@@ -2,7 +2,7 @@ exports.up = async function(knex) {
     await knex.schema.table('campaigns', function(table) {
         table.string('type', 255).after('id')
     })
-    await knex.raw('UPDATE campaigns SET type = IF(list_ids IS NULL, "trigger", "blast")')
+    await knex.raw('UPDATE campaigns SET type = IF(list_ids IS NULL, \'trigger\', \'blast\')')
 }
 
 exports.down = async function(knex) {
